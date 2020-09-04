1b8757b35b233778869883ecc385e6b6edfe8ae2
Danik165 <dkamireddy@mail.ru>
2020-09-04 12:55:28 +0300

    Дз номер 3

--- 3_lesson_1.py
+++ 3_lesson_1.py
@@ -1,2 +1,10 @@
-a = int(input())
-print(a)
+def division(*args):
+    del_1 = int(input('Делимое: '))
+    del_2 = int(input('Делитель: '))
+    try:
+        return del_1 / del_2
+    except ZeroDivisionError or ArithmeticError:
+        return "Нельзя делить на 0"
+
+
+print(division())
